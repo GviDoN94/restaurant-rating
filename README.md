7c1e9f886d699a3b1c5eb62f6b61f1ca5dd1a7bb
Vladislav Surin <vlad_surin@mail.ru>
2024-02-25 23:22:48 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+## Запуск
+
+1. Install dependencies:
+
+   ```bash
+   npm install
+   ```
+
+2. Compile for Development:
+
+   ```bash
+   npm run dev
+   ```
---
+++ src/App.tsx
@@ -0,0 +1,35 @@
+import StarIcon from '@/assets/star.svg?react';
+import LogoIcon from '@/assets/stair.svg?react';
+import '@/styles.css';
+
+const App = () => (
+  <>
+    <header>
+      <div className="logo">
+        <LogoIcon
+          width={16}
+          height={16}
+          className="logo__icon"
+        />
+        <span>Eats</span>
+      </div>
+      <div className="profile">
+        <img
+          alt="profile"
+          src="/avatar.png"
+        />
+      </div>
+    </header>
+    <main>
+      <input placeholder="Search for restaurants" />
+      <section></section>
+    </main>
+    <footer>
+      <p>Privacy Policy</p>
+      <p className="corporation">2024 Eats</p>
+      <p>Terms Of Service</p>
+    </footer>
+  </>
+);
+
+export default App;
